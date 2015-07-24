b00d0e34bd9aa14431be21ab336fc635d0e965ee
Haley Ressl <haleyressl@gmail.com>
2015-07-23 20:50:17 -0500

    ryu throwing position accomplished

--- js/app.js
+++ js/app.js
@@ -11,14 +11,9 @@ $(document).ready(function() {
     // play hadouken sound
     $('.ryu-ready').hide();
     $('.ryu-throwing').show();
-   $('.hadouken').show().animate(
-  {'left': '1020px'},
-  500,
-  function() {
-    $(this).hide();
-    $(this).css('left', '520px');
-  }
-);
+    $('.hadouken').show();
+    // animate hadouken to the right of the screen
+  })
   .mouseup(function() {
     $('.ryu-throwing').hide();
     $('.ryu-ready').show();
